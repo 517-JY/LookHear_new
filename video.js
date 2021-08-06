3d47b69b89afd917cb2a2e14a2cb1ce29e74a696
Jiayin Li <jiayin.li007@outlook.com>
2021-08-06 00:00:21 -0400

    adds volume and time display features

--- video.js
+++ video.js
@@ -1,16 +1,40 @@
 const videoPlayer = document.querySelector('.video-player');
-
 const video = videoPlayer.querySelector('.video');
-
 const playButton = videoPlayer.querySelector('.play-button');
+const volume = videoPlayer.querySelector('.volume');
 
-playButton.addEventListener('click', (e) => {
+const currentTimeElement = videoPlayer.querySelector('.current');
+const durationTimeElement = videoPlayer.querySelector('.duration');
+
+playButton.addEventListener('click', (event) => {
     if (video.paused) {
         video.play();
-        e.target.textContent = '⏸';
+        event.target.textContent = '⏸';
     } else {
         video.pause();
-        e.target.textContent = '▶️';
+        event.target.textContent = '▶️';
 
     }
 })
+
+// volume
+volume.addEventListener('mousemove', (event) => {
+    //console.log(event);
+    //console.log(event.target.value);
+    // the volume of the video is whatever we put in the range
+    video.volume = event.target.value;
+})
+
+// current time and duration
+const currentTime = () => {
+    let currentMinutes = Math.floor(video.currentTime / 60);
+    let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(video.duration / 60);
+    let durationSeconds = Math.floor(video.duration - durationMinutes * 60);
+
+    currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0' + currentSeconds : currentSeconds}`
+    durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`
+}
+
+
+video.addEventListener('timeupdate', currentTime);
